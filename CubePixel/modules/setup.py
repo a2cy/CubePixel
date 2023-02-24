a4815f48af0482c51cea9cc464c8791df7840f19
y2bad <75477334+y2bad@users.noreply.github.com>
2023-02-24 23:19:01 +0100

    Delete setup.py

--- CubePixel/modules/setup.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules=cythonize("./modules/cython_functions.pyx"),
-    include_dirs=[np.get_include()]
-) 
